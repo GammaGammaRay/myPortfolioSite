1ad987881d9bc18e7655f0d11b3f33f46ec5b11d
BenPeret <benjamin.peret@gmail.com>
2015-10-21 21:43:14 -0200

    navbar aligned left 120px margin

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the inConstructionApp
  */
 angular.module('inConstructionApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', [ '$scope', function ($scope) {
+    $scope.awesomeThings = [
+      {
+        title:'HTML5 Boilerplate',
+        content: "  HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites."
+      },
+      {
+        title:'Contact',
+        content: "123"
+      },
+      {
+        title:'Karma',
+        content: "Is a bitch"
+      }
     ];
-  });
+    $scope.saveThing = function() {
+      var newObj = {
+        title: $scope.newThing,
+        content : $scope.newContent
+      }
+
+    $scope.awesomeThings.push(newObj)
+    }
+  }]);
