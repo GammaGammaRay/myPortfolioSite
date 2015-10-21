b69a7f1d24b73f69d4ed540018bcff7f6caeba60
BenPeret <benjamin.peret@gmail.com>
2015-10-21 16:49:38 -0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inConstructionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inConstructionApp
+ */
+angular.module('inConstructionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inConstructionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inConstructionApp
+ */
+angular.module('inConstructionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
